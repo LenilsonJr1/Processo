2ef4843b02fd85fa400abe287377e1e4e70bbf51
lenilson tomaz de oliveira junior <lenilsonjnr@gmail.com>
2019-11-28 23:36:17 -0300

    Trabalho de processo

---
+++ scripts/processElementData.js
@@ -0,0 +1,28 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_tRVggRJCEeqEXKumhdXd8QActivity"]="./../../Generic Game Deveopment/deliveryprocesses/resources/Generic Game Deveopment_5D825BB2_5d825bb2_Activity.jpeg"
+contentPage.processPage.elementUrls["_04cb0BJCEeqEXKumhdXd8Q"] = ["Decidir estratégias de monetização","Generic Game Deveopment/deliveryprocesses/decidir_estratgias_de_monetizao_A0FE89AD.html"];
+contentPage.processPage.elementUrls["_yl0R4BJCEeqEXKumhdXd8Q"] = ["Desenvolvimento de conceito do jogo","Generic Game Deveopment/deliveryprocesses/desenvolvimento_de_conceito_do_jogo_D0FA2CBB.html"];
+contentPage.processPage.elementUrls["_2XzVwBJCEeqEXKumhdXd8Q"] = ["Suporte e manutenção","Generic Game Deveopment/deliveryprocesses/suporte_e_manuteno_238FE925.html"];
+contentPage.processPage.elementUrls["_vYL4QBJCEeqEXKumhdXd8Q"] = ["Criar uma ideia inovadora","Generic Game Deveopment/deliveryprocesses/criar_uma_ideia_inovadora_CE515F94.html"];
+contentPage.processPage.elementUrls["_vy3CEBJCEeqEXKumhdXd8Q"] = ["Criar a história do jogo","Generic Game Deveopment/deliveryprocesses/criar_a_histria_do_jogo_98C74148.html"];
+contentPage.processPage.elementUrls["_yl0R4RJCEeqEXKumhdXd8Q"] = ["Gerente","Generic Game Deveopment/deliveryprocesses/gerente_DB980CCB.html"];
+contentPage.processPage.elementUrls["_zGJ-0BJCEeqEXKumhdXd8Q"] = ["Criar o desing do jogo","Generic Game Deveopment/deliveryprocesses/criar_o_desing_do_jogo_3FEC87FA.html"];
+contentPage.processPage.elementUrls["_tRVggRJCEeqEXKumhdXd8Q"] = ["Generic Game Deveopment","Generic Game Deveopment/deliveryprocesses/Generic Game Deveopment_5D825BB2.html"];
+contentPage.processPage.elementUrls["_0FxowBJCEeqEXKumhdXd8Q"] = ["Implementação e teste","Generic Game Deveopment/deliveryprocesses/implementao_e_teste_7A230FF0.html"];
+contentPage.processPage.elementUrls["_0FxowRJCEeqEXKumhdXd8Q"] = ["Desenvolvedor","Generic Game Deveopment/deliveryprocesses/desenvolvedor_84C0F000.html"];
+contentPage.processPage.elementUrls["_0FxowxJCEeqEXKumhdXd8Q"] = ["Testador","Generic Game Deveopment/deliveryprocesses/testador_7DF7E426.html"];
+contentPage.processPage.elementUrls["_1uO0UBJCEeqEXKumhdXd8Q"] = ["Lançamento do jogo","Generic Game Deveopment/deliveryprocesses/lanamento_do_jogo_7F84716A.html"];
+contentPage.processPage.elementUrls["_vy3CERJCEeqEXKumhdXd8Q"] = ["Criador de histórias","Generic Game Deveopment/deliveryprocesses/criador_de_histrias_A3652158.html"];
+contentPage.processPage.elementUrls["_vy3CExJCEeqEXKumhdXd8Q"] = ["Desing","Generic Game Deveopment/deliveryprocesses/desing_9C9C157E.html"];
